71a5a1f02b2bf323b5ee41c1478405b2c3ffc8f4
Hoon Choi <hoon@orcatown.com>
2025-05-12 15:51:46 -0400

    styled filter list

--- app/components/FiltersList.tsx
+++ app/components/FiltersList.tsx
@@ -3,38 +3,60 @@ import { $pointFilters, removeFilter } from '~/store/points'
 
 export function FiltersList() {
   const pointFilters = useStore($pointFilters)
+
+  function getDispText(text: string) {
+    const transText: Record<string, string> = {
+      includeOnly: 'Include only',
+      colour: 'Colour',
+      enb: 'eNB',
+      gnb: 'gNB',
+      cellNo: 'Cell no.',
+      eutraBand: 'e-UTRA band',
+      nrBand: 'NR band',
+      signalStrength: 'Signal strength',
+    }
+    return transText[text] ?? text
+  }
+
   return (
     <div>
       <h3>Filters list</h3>
       {pointFilters.length === 0 && (
         <p className="ml-2 text-sm text-gray-300">No filters added</p>
       )}
-      {pointFilters.map((f) => (
-        <div key={f.id} className="w-fit">
-          <span className="mr-2">
-            mode: {f.mode}, type: {f.type}, values: {f.values.join(', ')}
-          </span>
-          {f.colour && (
-            <div
-              style={{
-                display: 'inline-block',
-                width: '1em',
-                height: '1em',
-                backgroundColor: f.colour,
-              }}
-              className="mr-2"
-            ></div>
-          )}
-          <button
-            className="bg-violet-50 text-violet-700 hover:bg-violet-100
+      <ul>
+        {pointFilters.map((f) => (
+          <li
+            key={f.id}
+            className="w-fit text-sm before:content-['{}'] before:mr-1"
+          >
+            <span className="mr-2">
+              {getDispText(f.mode)} {getDispText(f.type)} values{' '}
+              {f.values.join(', ')}
+            </span>
+            {f.colour && (
+              <div
+                style={{
+                  display: 'inline-block',
+                  width: '0.9em',
+                  height: '0.9em',
+                  backgroundColor: f.colour,
+                }}
+                className="mr-2"
+              ></div>
+            )}
+
+            <button
+              className="bg-violet-50 text-violet-700 hover:bg-violet-100
                 focus:outline-none focus:ring-2 focus:ring-violet-200
                 py-[0.25em] px-2 rounded-lg text-xs font-semibold"
-            onClick={() => removeFilter(f.id)}
-          >
-            Remove
-          </button>
-        </div>
-      ))}
+              onClick={() => removeFilter(f.id)}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
